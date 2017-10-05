d82d47d72008b9fb8e8e94c1c71cecd7aaef318c
Kevin Dunn <dunn0407@umn.edu>
2017-10-04 20:13:24 -0500

    upload matlab stuff

--- csci2033/my_reff.m
+++ csci2033/my_reff.m
@@ -1,14 +1,19 @@
 function A = my_reff(A)
-for y = 1:(size(A,2)-1)
+d = 0;
+for y = 1:(size(A,1))
     %pick max value of column below all finished rows
-    [~,index] = max(abs(A(y:(size(A,1)),y)));
+    [val,index] = max(abs(A(y:(size(A,1)),y+d)));
     %interchange abs max pivot column to current one
-    A = interchange(A,y,index);
+    while (val == 0) && ((y + d) < (size(A,2)-1))
+        d = d + 1;
+        [val,index] = max(abs(A(y:(size(A,1)),y+d)));
+    end
+    A = interchange(A,y,index + y - 1);
     %scale pivot to 1
-    A = scaling(A,y,1/A(y,y));
+    A = scaling(A,y,1/A(y,y+d));
     for x = y:(size(A,1)-1)
         %removing values below row in column
-        A = replacement(A,x+1,y,-A(x+1,y));
+        A = replacement(A,x+1,y,-A(x+1,y+d));
     end
     %gauss-jordan elim once done with column bwlow
     j = y-1;
@@ -17,3 +22,4 @@ for y = 1:(size(A,2)-1)
         j = j - 1;
     end
 end
+disp(A);
--- csci2033/my_rref.m
+++ csci2033/my_rref.m
@@ -1,16 +1,19 @@
 function A = my_rref(A)
 d = 0;
-for y = 1:(size(A,2)-1)
+%y is column number
+for y = 1:(size(A,1))
     %pick max value of column below all finished rows
     [val,index] = max(abs(A(y:(size(A,1)),y+d)));
     %interchange abs max pivot column to current one
-    while (val == 0) && (y + d < size(A,2)-1)
+    %if the columns are not all zeros
+    while (val == 0) && ((y + d) < (size(A,2)-1))
         d = d + 1;
         [val,index] = max(abs(A(y:(size(A,1)),y+d)));
     end
     A = interchange(A,y,index + y - 1);
     %scale pivot to 1
     A = scaling(A,y,1/A(y,y+d));
+    %x is row
     for x = y:(size(A,1)-1)
         %removing values below row in column
         A = replacement(A,x+1,y,-A(x+1,y+d));
