bfb39d45abbbbc9ec477df12f36db381f9ee9bf0
Kevin Dunn <dunn0407@umn.edu>
2017-11-30 21:23:38 -0600

    fin

--- csci2033/assign3/affine_fit.m
+++ csci2033/assign3/affine_fit.m
@@ -2,6 +2,7 @@ function [M,t] = affine_fit(P,P_tilde)
 
 Ai = design_matrix(P);
 
+
 beta = least_squares(Ai,P_tilde);
 
 [M,t] = my_unpack(beta);
--- csci2033/assign3/my_unpack.m
+++ csci2033/assign3/my_unpack.m
@@ -1,5 +1,7 @@
 function [M,t] = my_unpack(beta)
 
-M = transpose(reshape(beta(1:4),2,2));
 t(1,1) = beta(5,1);
-t(1,2) = beta(6,1);
+t(2,1) = beta(6,1);
+
+M = transpose(reshape(beta(1:4),2,2));
+end
